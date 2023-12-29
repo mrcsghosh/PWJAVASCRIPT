2d333679c0ada5c251372144ff9b3254f2776002
Chandrashekhar Ghosh <145031494+mrcsghosh@users.noreply.github.com>
2023-12-29 12:59:30 +0000

    Learn About console and variables

---
+++ basic1/variable.js
@@ -0,0 +1,11 @@
+//variables
+//constant
+//const email = "mrcs@cs.com"
+//let email = "mrcs@cs.com"
+let UserEmail = "mrcs@cs.com";
+//let UserPassword = "1234"
+const Password = "1234";    
+
+Password = "11122"
+
+console.log(Password);
