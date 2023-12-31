b2731b0828e401928a895afb0e56e63d8b3d516c
Chandrashekhar Ghosh <145031494+mrcsghosh@users.noreply.github.com>
2023-12-31 12:23:14 +0000

    add Comparision Operators

---
+++ basic1/operators.js
@@ -0,0 +1,36 @@
+//Arthmatic Operators 
+//5+5
+//2+2
+//10/2
+//5*5
+//8-4
+//2**3 => 2*2*2
+//10%3
+
+let valueOne = 3
+valueOne++
+valueOne = valueOne + 1
+
+
+const numberOne = "2"
+const numberTwo = "2"
+
+//const numberOne = "Hello"
+//const numberTwo = "Mrcs"
+
+const numberThree = numberOne + numberTwo
+console.log(numberThree);
+
+
+//2+((2*4)-5)/4+10-2
+
+//Assignment Operators 
+let valueTwo = 3
+valueTwo += 10
+
+//Comparision Operators
+let pw = 3
+//let pwSkills = 3
+let pwSkills = "3"
+ 
+console.log(pw === pwSkills); 
