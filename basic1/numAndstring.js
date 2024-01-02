cd228e62e48b71dd0f3d29bfac49de64973f09ed
Chandrashekhar Ghosh <145031494+mrcsghosh@users.noreply.github.com>
2024-01-02 04:32:53 +0000

    NUMBER Discution

---
+++ basic1/numAndstring.js
@@ -0,0 +1,42 @@
+//WE USE STRINGS HERE
+
+/*const firstName = "Hello"
+const lastName = "Shekhar"
+
+console.log(firstName + ' ' + lastName);
+console.log('My firstname is ${firstName} and My lastname is ${lastName}');
+
+
+const userName = new String('mrcsdotcom')
+console.log(userName[3]);  //in [] we rovide our Choice what word we want to print in this sentance
+console.log(userName .length); //we use for find the length of the sentence or username
+//console.log(userName.prototype.CharAt); // you dont want to use prototype we use directly 
+console.log(userName.charAt(2));
+console.log(userName.substring(0,4));
+//WE CAN CREATE A SPACE AND SHOW TO THE USER INTERFACE
+const email = " mrcs@cs.com         "
+//console.log(email);
+console.log(email.trim());// for ending all space from start and end side
+//HERE WE USE REPLACE FOR REPLACE THE VALUE
+const emailTwo = "mrcs@cs.com"
+console.log(emailTwo.replace('mrcs', 'jagrati'));//replace the value mrcs to jagrati
+console.log(email.includes('cs'));//here we find "cs" is included in the email or emailTwo
+
+//when we want to email show in uppercase sentence
+console.log(emailTwo.toUpperCase());
+*/
+
+
+//WE USE NUMBERS HERE
+const num = 5
+//const numTwo = new Number(5)
+const numTwo = new Number(5.4535465)
+console.log(num .toString(2));//that will be provide 
+console.log(numTwo .toFixed(2));//that will be used foe provide decimal number for more than 2
+const subscribers = 100000
+console.log(subscribers.toLocaleString());//that we be provide a localstring and suitable to read 
+
+console.log(Number.MAX_VALUE);//provide max_value of numbers
+//console.log(Math.random());//we take the value of random means between 0 to 1
+//console.log(Math.random()*10);//now in between 1 to 9.9 (10)
+console.log(Math.floor(Math.random()*10));//now in between 0 to 9 but only intiger not a decimal
