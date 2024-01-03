3a1394dcf6ff07aeef0fa62ddc81c0094b7854dd
mrcsghosh <0810chandrashekhar@gmail.com>
2024-01-03 09:24:25 +0530

    Add Basic JS

---
+++ BsicJS/datatype.js
@@ -0,0 +1,46 @@
+//primitive (STACK)
+/*
+String
+Boolean
+Number
+Intiger
+BigInt
+Undefined
+Symbols
+NULL
+*/
+const firstname = "Shekhar"
+const age = 20
+const score = 200
+let isLogedIn = true
+const NodeVersion = NULL
+let BankBalance = Undefined
+const ButtonId = Symbol("id")
+const aReallyLongNumber = 654646548454445454n
+/*
+2+2 = 4;
+"2"+"2" = "22";
+*/
+
+//non primitive or reference - Objects (HEAP)
+
+//Arrays
+const Arrays = []
+const number = [1, 2, 3, 4];
+const superHeros = ["Batman", "Laghtman", "Siderman", 
+true, 123];
+
+//Objects
+const Objects = {}
+const userObject = {
+name : "Shekhar",
+age : 20,
+isLogedIn : true}
+
+//Function (Letrals)
+const sayHello = function(){
+    console.log("mrcs");
+}
+
+const registerAUser = function(){}
+const loginUser = function(){}
